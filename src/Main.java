d9c903cbe4f784ad2eea676feb37687e5abe6591
emirkaramoldoev <emirkaramoldoev1@gmail.com>
2023-06-15 16:07:31 +0600

    Переделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws LimitException {
+    public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount(0);
         System.out.println("Текущий остаток " + bankAccount.getAmount());
         bankAccount.deposit(15000.0);
@@ -8,15 +8,22 @@ public class Main {
         while (true) {
             try {
                 bankAccount.withDraw(6000);
+                System.out.println("Текущий остаток " + bankAccount.getAmount());
             }
             catch (LimitException ie) {
                 System.out.println(ie.getMessage());
-                Double remainingAmount = ie.getRemainingAmount();
-                remainingAmount = bankAccount.getAmount();
-                bankAccount.withDraw(remainingAmount);
-                System.out.println("Вы сняли " + remainingAmount);
-                System.out.println("Текущий остаток " + bankAccount.getAmount());
-                break;
+                try {
+                    bankAccount.withDraw(bankAccount.getAmount());
+                }
+                catch (LimitException ex) {
+                    Double remainingAmount = ie.getRemainingAmount();
+                    remainingAmount = bankAccount.getAmount();
+                    System.out.println("Вы сняли " + remainingAmount);
+                    System.out.println("Текущий остаток " + bankAccount.getAmount());
+                    break;
+                }
+            System.out.println("Текущий остаток " + bankAccount.getAmount());
+            break;
             }
         }
     }
